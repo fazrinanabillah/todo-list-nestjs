e4b0906d194c827e99c75a81ecf15ebba2c6ca9a
fazrinanabillah <fazrinabillah@gmail.com>
2025-07-26 09:52:17 +0700

    feat: add logger when handling request

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Logger, Post } from '@nestjs/common';
 import { Todo } from './todo.interface';
 import { TodoService } from './todo.service';
 
@@ -6,14 +6,18 @@ import { TodoService } from './todo.service';
 export class TodoController {
   @Inject()
   private readonly todoService: TodoService;
+  private readonly logger = new Logger(TodoController.name);
+
 
   @Get()
   findAll(): Todo[] {
+    this.logger.log("handling finda all request...")
     return this.todoService.findAll();
   }
 
   @Post()
   create(@Body() todo: Todo) : void {
+    this.logger.log("handling create request...")
     return this.todoService.create(todo);
   }
 }
